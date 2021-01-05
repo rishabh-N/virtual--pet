0460f80aa08975743f9561184980b15ec0fc0d98
rishabh-N <74763186+rishabh-N@users.noreply.github.com>
2021-01-05 23:05:07 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var happyDog
+var Dog
+var database
+var foodS
+
+function preload()
+{
+  Dog = "images/dogImg.png"
+  happyDog = "images/Img1.png"
+}
+
+function setup() {
+	createCanvas(500, 500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
